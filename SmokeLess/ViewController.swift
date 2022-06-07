a43d24c7ba81d1d7b05ecbbadfd5d37ae5a46665
Kevin Ahmad <kevinahmad.f@gmail.com>
2022-06-07 16:44:05 +0700

    feat: add xcode template

---
+++ SmokeLess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmokeLess
+//
+//  Created by Kevin ahmad on 07/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

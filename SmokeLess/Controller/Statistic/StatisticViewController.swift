dc0404927b266910ae50500df05631134c796b13
hadamelino <hadamuhammad@gmail.com>
2022-06-20 15:21:14 +0700

    SST-35 <feat: Change background color for View to brown>

--- SmokeLess/Controller/Statistic/StatisticViewController.swift
+++ SmokeLess/Controller/Statistic/StatisticViewController.swift
@@ -11,7 +11,7 @@ class StatisticViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBrown
         // Do any additional setup after loading the view.
     }
     

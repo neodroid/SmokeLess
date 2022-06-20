9babb2a6619bc30de4d8946182d8ab4a478b3212
hadamelino <hadamuhammad@gmail.com>
2022-06-20 15:06:17 +0700

    SST-35 <feat: Add StatisticViewController>

---
+++ SmokeLess/Controller/Statistic/StatisticViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StatisticViewController.swift
+//  SmokeLess
+//
+//  Created by Hada Melino Muhammad on 20/06/22.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

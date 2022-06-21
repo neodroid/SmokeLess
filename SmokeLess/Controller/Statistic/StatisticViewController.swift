dfb46ed258bb6bf2e1eab923d694e2c359a681db
hadamelino <hadamuhammad@gmail.com>
2022-06-21 11:14:08 +0700

    feat: Add title label (Statistic, cigarette summary, month label) and change month button

--- SmokeLess/Controller/Statistic/StatisticViewController.swift
+++ SmokeLess/Controller/Statistic/StatisticViewController.swift
@@ -8,12 +8,67 @@
 import UIKit
 
 class StatisticViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    let statisticLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "Statistic"
+        theLabel.font = .systemFont(ofSize: 34, weight: .bold)
+        return theLabel
+    }()
+    
+    let cigarreteSummaryLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "Cigarette Summary"
+        theLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        return theLabel
+    }()
+    
+    let changeMonthButton: UIButton = {
+        let theButton = UIButton()
+        theButton.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        theButton.tintColor = .black
+        return theButton
+    }()
+    
+    let changeMonthLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "June 2022"
+        theLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        return theLabel
+    }()
+    
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBrown
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        configureUI()
     }
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.addSubview(statisticLabel)
+        view.addSubview(cigarreteSummaryLabel)
+        view.addSubview(changeMonthButton)
+        view.addSubview(changeMonthLabel)
+        
+        statisticLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
+        
+        cigarreteSummaryLabel.anchor(top: statisticLabel.bottomAnchor, left: statisticLabel.leftAnchor, paddingTop: 20)
+                
+        changeMonthLabel.anchor(top: cigarreteSummaryLabel.topAnchor, left: cigarreteSummaryLabel.rightAnchor)
+        
+        changeMonthButton.anchor(top: cigarreteSummaryLabel.topAnchor, left: changeMonthLabel.rightAnchor, right: statisticLabel.rightAnchor, paddingLeft: 5)
+        
+    }
+    
     
 
 }

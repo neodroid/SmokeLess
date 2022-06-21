cfbf0eda5878785bfc6dd06390a9a07bb4ffab2b
hadamelino <hadamuhammad@gmail.com>
2022-06-21 13:29:11 +0700

    feat: add and configure UI elements for cigarettes limit and cigarettes consumed

--- SmokeLess/Controller/Statistic/StatisticViewController.swift
+++ SmokeLess/Controller/Statistic/StatisticViewController.swift
@@ -52,8 +52,83 @@ class StatisticViewController: UIViewController {
         
         theStack.addArrangedSubview(theImage)
         theStack.addArrangedSubview(theLabel)
+        theStack.spacing = 5
         
         return theStack
+    }()
+    
+    let hStack: UIStackView = {
+        let theStack = UIStackView()
+        theStack.axis = .horizontal
+        theStack.distribution = .fillEqually
+        theStack.spacing = 50
+        return theStack
+    }()
+    
+    let vStackLimit: UIStackView = {
+        let theStack = UIStackView()
+        theStack.axis = .vertical
+        theStack.spacing = 20
+        return theStack
+    }()
+    
+    let limitLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "Cigarettes\nLimit"
+        theLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        theLabel.numberOfLines = 2
+        return theLabel
+    }()
+    
+    let limitNumber: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "13"
+        theLabel.font = .systemFont(ofSize: 45, weight: .regular)
+        return theLabel
+    }()
+    
+    let vStackConsumed: UIStackView = {
+        let theStack = UIStackView()
+        theStack.axis = .vertical
+        theStack.spacing = 20
+        return theStack
+    }()
+    
+    let consumedLabel: UILabel = {
+        let theLabel = UILabel()
+        theLabel.text = "Cigarettes\nConsumed"
+        theLabel.font = .systemFont(ofSize: 18, weight: .semibold)
+        theLabel.numberOfLines = 2
+        return theLabel
+    }()
+    
+    
+    let consumedStackForStatus = UIView()
+    
+    let statusImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "checkmark.seal.fill"))
+        imageView.tintColor = .smokeLessGreen
+        imageView.contentMode = .scaleAspectFit
+        imageView.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        return imageView
+    }()
+
+    let consumedNumber: UILabel = {
+        let theLabel = UILabel()
+        theLabel.translatesAutoresizingMaskIntoConstraints = false
+        theLabel.text = "13"
+        theLabel.textColor = .smokeLessGreen
+        theLabel.font = .systemFont(ofSize: 45)
+        return theLabel
+    }()
+    
+    let cigarettesLimitConsumedView: UIView = {
+        var theView = UIView()
+        theView.backgroundColor = .white
+        theView.layer.cornerRadius = 20
+        theView = addShadowToView(viewToAdd: theView, shadowColor: UIColor.black.cgColor, shadowRadius: 7, shadowOpacity: 0.2, shadowOffset: CGSize(width: 0, height: 4))
+        return theView
     }()
     
     
@@ -67,15 +142,37 @@ class StatisticViewController: UIViewController {
         configureUI()
     }
     
+    override func viewDidLayoutSubviews() {
+    }
     
     // MARK: - Helpers
     
     func configureUI() {
+        // Setup Limit Counter View
+        vStackLimit.addArrangedSubview(limitLabel)
+        vStackLimit.addArrangedSubview(limitNumber)
+        
+        // Setup Consumed Counter View
+        vStackConsumed.addArrangedSubview(consumedLabel)
+        vStackConsumed.addArrangedSubview(consumedStackForStatus)
+        consumedStackForStatus.addSubview(consumedNumber)
+        consumedStackForStatus.addSubview(statusImage)
+        consumedNumber.anchor(top:consumedStackForStatus.topAnchor, left: consumedStackForStatus.leftAnchor, bottom: consumedStackForStatus.bottomAnchor)
+        statusImage.anchor(left: consumedNumber.rightAnchor, bottom: consumedNumber.bottomAnchor, paddingLeft: 5, paddingBottom: 10)
+        
+        hStack.addArrangedSubview(vStackLimit)
+        hStack.addArrangedSubview(vStackConsumed)
+        
+        // Wrap the hStack to UIView for shadow
+        cigarettesLimitConsumedView.addSubview(hStack)
+        hStack.anchor(top: cigarettesLimitConsumedView.topAnchor, left: cigarettesLimitConsumedView.leftAnchor, bottom: cigarettesLimitConsumedView.bottomAnchor, right: cigarettesLimitConsumedView.rightAnchor, paddingTop: 20, paddingLeft: 30, paddingBottom: 20)
+        
         view.addSubview(statisticLabel)
         view.addSubview(cigarreteSummaryLabel)
         view.addSubview(changeMonthButton)
         view.addSubview(changeMonthLabel)
         view.addSubview(dayStatusStack)
+        view.addSubview(cigarettesLimitConsumedView)
         
         statisticLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
         
@@ -90,8 +187,20 @@ class StatisticViewController: UIViewController {
         
         dayStatusStack.anchor(top: cigarreteSummaryLabel.bottomAnchor, left: statisticLabel.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingRight: 20)
         
+        cigarettesLimitConsumedView.anchor(top: dayStatusStack.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingRight: 20)
+        
+        
     }
     
     
+    
 
+}
+
+public func addShadowToView(viewToAdd: UIView, shadowColor: CGColor, shadowRadius: CGFloat, shadowOpacity: Float, shadowOffset: CGSize) -> UIView {
+    viewToAdd.layer.shadowColor = shadowColor
+    viewToAdd.layer.shadowRadius = shadowRadius
+    viewToAdd.layer.shadowOpacity = shadowOpacity
+    viewToAdd.layer.shadowOffset = shadowOffset
+    return viewToAdd
 }

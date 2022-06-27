35776d37d01638434eee525c2683b83ab3931f00
billaza <nabillazarine@gmail.com>
2022-06-27 14:05:23 +0700

    feat: add breathing + timer page (not fixed)

--- SmokeLess/Controller/Breath/FeedbackViewController.swift
+++ SmokeLess/Controller/Breath/FeedbackViewController.swift
@@ -1,44 +1,34 @@
 //
-//  StartBreathViewController.swift
+//  FeedbackViewController.swift
 //  SmokeLess
 //
-//  Created by Nabilla Azarine on 24/06/22.
+//  Created by Nabilla Azarine on 27/06/22.
 //
 
 import UIKit
 
-class StartBreathViewController: UIViewController {
+class FeedbackViewController: UIViewController {
     
     // MARK: - Properties
     
     var titleLabel: UILabel = {
         var label: UILabel = UILabel()
-        label.text = "Deep Breath"
-        label.font = UIFont.boldSystemFont(ofSize: 34)
+        label.text = "Deep breath exercise finished"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
         label.textColor = .black
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
         return label
     }()
     
     var subtitleLabel: UILabel = {
         var label: UILabel = UILabel()
-        label.text = "Deep breathing means taking breaths that expand your stomach. It can lower stress level, reduce cravings, and improve lung function."
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.text = "How are you feeling after doing the breathing exercise?"
+        label.font = UIFont.systemFont(ofSize: 16)
         label.textColor = .black
         label.numberOfLines = 0
-        label.addInterlineSpacing(spacingValue: 2)
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
         return label
-    }()
-    
-    var image: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "deep-breath.png")
-        image.contentMode = .scaleToFill
-        return image
     }()
     
     // MARK: - Lifecycle
@@ -47,34 +37,21 @@ class StartBreathViewController: UIViewController {
         super.viewDidLoad()
         configureUI()
     }
-    @objc func buttonClicked() {
-        print("Button Clicked")
-    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
     }
     
-    
     // MARK: - Helpers
     
-    func createButton() -> UIButton.Configuration {
-        var config: UIButton.Configuration = .filled()
-        var container = AttributeContainer()
-        container.font = UIFont.boldSystemFont(ofSize: 20)
-        config.attributedTitle = AttributedString("Start", attributes: container)
-        //        config.title = "Start"
-        config.cornerStyle = .capsule
-        config.contentInsets = .init(top: 15, leading: 20, bottom: 15, trailing: 20)
-        config.baseBackgroundColor = .smokeLessBlue
-        config.titlePadding = 10
-        return config
-    }
-    
-    func configureUI(){
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        
+    private func configureUI() {
         view.backgroundColor = .white
+        
+        let choice1 = UIButton(frame: CGRect(x: 0, y: 0, width: 175, height: 50))
+        choice1.configuration = createChoice1()
+        choice1.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        choice1.center = view.center
+        
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 350, height: 50))
         button.configuration = createButton()
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
@@ -82,14 +59,37 @@ class StartBreathViewController: UIViewController {
         
         view.addSubview(titleLabel)
         view.addSubview(subtitleLabel)
-        view.addSubview(image)
+        view.addSubview(choice1)
         view.addSubview(button)
         
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 50, paddingLeft: 20, paddingRight: 20)
-        subtitleLabel.anchor(top:titleLabel.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 30, paddingLeft: 20, paddingRight: 20)
-        subtitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        image.anchor(top: subtitleLabel.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 50, paddingLeft: 40, paddingRight: 40, width: view.frame.size.width - 100, height: view.frame.size.width - 100)
+        subtitleLabel.anchor(top: titleLabel.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 20, paddingRight: 20)
+        choice1.anchor(top: subtitleLabel.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 20, paddingRight: 20)
         button.anchor(left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingTop: 20, paddingLeft: 20, paddingBottom: 20, paddingRight: 20)
+    }
+    
+    func createChoice1() -> UIButton.Configuration {
+        var config: UIButton.Configuration = .bordered()
+        var container = AttributeContainer()
+        container.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        config.attributedTitle = AttributedString("Continue", attributes: container)
+        config.cornerStyle = .capsule
+        config.contentInsets = .init(top: 15, leading: 20, bottom: 15, trailing: 20)
+        config.baseBackgroundColor = .smokeLessBlue
+        config.titlePadding = 10
+        return config
+    }
+    
+    func createButton() -> UIButton.Configuration {
+        var config: UIButton.Configuration = .filled()
+        var container = AttributeContainer()
+        container.font = UIFont.boldSystemFont(ofSize: 20)
+        config.attributedTitle = AttributedString("Continue", attributes: container)
+        config.cornerStyle = .capsule
+        config.contentInsets = .init(top: 15, leading: 20, bottom: 15, trailing: 20)
+        config.baseBackgroundColor = .smokeLessBlue
+        config.titlePadding = 10
+        return config
     }
     
 }
